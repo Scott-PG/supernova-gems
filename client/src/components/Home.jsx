5ea3b135951b352c8b971106959a2f0447b4393d
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-04-15 18:12:00 -0400

    updated hero css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import "./Home.css";
 import Layout from "./shared/Layout";
 import Products from "./Products";
+import Hero from "./products/Hero";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
       {/* <div className="home"> */}
-      <img src="https://i.imgur.com/AKdJFKV.jpg" width="100%" />
-      <Products jCollection="women" featured={true} />
+      {/* <img src="https://i.imgur.com/AKdJFKV.jpg" width="100%" /> */}
       {/* </div> */}
+      debugger;
+      <Hero
+        heroImg="https://i.imgur.com/AKdJFKV.jpg"
+        heroTitle="Mothers Day Sale"
+        heroTitle2="Give your Rock a Diamond"
+        cName="leftTitlePosition"
+      />
+      {/* Section under the hero */}
+      <div className="gridHeadingDivider">
+        <div className="textDivider">Ultimate Elegnce</div>
+        <span className="textDividertag">Best Sellers</span>
+      </div>
+      <Products jCollection="women" featured={true} />
     </Layout>
   );
 };
