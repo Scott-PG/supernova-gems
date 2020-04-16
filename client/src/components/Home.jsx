bdcc9c34dee37044b52446a2664dd1c1ff049c9c
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-04-15 22:32:02 -0400

    added homepage styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,14 +3,11 @@ import "./Home.css";
 import Layout from "./shared/Layout";
 import Products from "./Products";
 import Hero from "./products/Hero";
+import featuredDividerImg from "../Images/FeatureDivImg.jpeg";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
-      {/* <div className="home"> */}
-      {/* <img src="https://i.imgur.com/AKdJFKV.jpg" width="100%" /> */}
-      {/* </div> */}
-      debugger;
       <Hero
         heroImg="https://i.imgur.com/AKdJFKV.jpg"
         heroTitle="Mothers Day Sale"
@@ -19,10 +16,15 @@ const Home = (props) => {
       />
       {/* Section under the hero */}
       <div className="gridHeadingDivider">
-        <div className="textDivider">Ultimate Elegnce</div>
+        <div className="textDivider">Ultimate Elegance</div>
         <span className="textDividertag">Best Sellers</span>
       </div>
       <Products jCollection="women" featured={true} />
+      <div className="featuredDivider">
+        <div className="featuredDividerdiv">Quarantine... Where she going?</div>
+        <span className="featuredDividertag">Instagram!</span>
+      </div>
+      <img src={featuredDividerImg} />
     </Layout>
   );
 };
